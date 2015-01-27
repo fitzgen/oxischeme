cb82c259387f60d7c9c992a8e39689d60f0b2ef6
Nick Fitzgerald <fitzgen@gmail.com>
2015-01-26 19:17:31 -0800

    Update docs to: c192555 Fixes so we successfully compile on rustc 1.0.0-nightly (458a6a2f6 2015-01-25 21:20:37 +0000)

---
+++ implementors/core/fmt/trait.Display.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['oxischeme'] = ["<a class='stability Unstable' title='Unstable: I/O and core have yet to be reconciled'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Display.html' title='core::fmt::Display'>Display</a> for <a class='struct' href='oxischeme/eval/struct.Meaning.html' title='oxischeme::eval::Meaning'>Meaning</a>","<a class='stability Unstable' title='Unstable: I/O and core have yet to be reconciled'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Display.html' title='core::fmt::Display'>Display</a> for <a class='enum' href='oxischeme/value/enum.Value.html' title='oxischeme::value::Value'>Value</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
